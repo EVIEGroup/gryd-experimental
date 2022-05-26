92bb86e103c16fc01145c4603fbac57e1a37d5c7
Craig Whiteside <upperfoot@gmail.com>
2022-05-26 13:58:28 +0100

    Updated

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     // devtool: 'inline-source-map',
     devServer: {
-        https: true,
+        https: false,
         allowedHosts: 'all',
     },
     target: ["web"],
